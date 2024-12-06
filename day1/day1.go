0d93a5dbe679b5612382a3b82c1a16ede3443176
little-junior <eulittlejunior@gmail.com>
2024-12-05 22:45:22 -0300

    day 1 commit

---
+++ day1/day1.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var leftList []int
+	var rightList []int
+
+	file, err := os.Open("./day1_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, "   ")
+		leftNumber, _ := strconv.Atoi(numbers[0])
+		rightNumber, _ := strconv.Atoi(numbers[1])
+		leftList = append(leftList, leftNumber)
+		rightList = append(rightList, rightNumber)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	slices.Sort(leftList)
+	slices.Sort(rightList)
+
+	firstResult := firstStar(leftList, rightList)
+	secondResult := secondStart(leftList, rightList)
+
+	fmt.Printf("First star result : %d\n", firstResult)
+	fmt.Printf("Second star result : %d\n", secondResult)
+}
+
+func firstStar(leftList, rightList []int) (count int) {
+	for i := range leftList {
+		res := math.Abs((float64(leftList[i] - rightList[i])))
+
+		count += int(res)
+	}
+
+	return count
+}
+
+func secondStart(leftList, rightList []int) (count int) {
+	for i := range leftList {
+		occu := 0
+		for j := range rightList {
+			if rightList[j] == leftList[i] {
+				occu++
+			}
+		}
+		count += leftList[i] * occu
+	}
+
+	// another solution
+	// countMap := make(map[int]int)
+	// for _, i := range rightList {
+	// 	countMap[i]++
+	// }
+
+	// for _, j := range leftList {
+	// 	count += j * countMap[j]
+	// }
+
+	return count
+}
